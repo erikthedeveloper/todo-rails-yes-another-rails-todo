262f4705af5b607f1e133af1318ee237d4c76d65
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-03 08:52:50 -0700

    Adds/Updates user views. Passes data in from UsersCtrl
    - views/users/{index, show}

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < Devise::RegistrationsController
+
+  before_action :set_users
+
+  before_action :set_user, only: :show
+
   def index
     @users = User.all
+    @tasks = Task.all
   end
 
+  def show
+
+  end
+
+  private
+
+    def set_users
+      @users = User.includes(:tasks).all
+    end
+
+    def set_user
+      @user = User.includes(:tasks).find(params[:id])
+    end
   # Also see DeviseController
 end
