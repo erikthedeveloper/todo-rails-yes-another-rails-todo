05bedbc3c24882dcc1dc7a034d3bfc594b426af4
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-05 01:36:08 -0700

    Updates model, form, controller, and views for image upload. Success!?

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :created_by_user, class_name: 'User', foreign_key: 'created_by'
   belongs_to :assigned_to_user, class_name: 'User', foreign_key: 'assigned_to'
 
+  mount_uploader :image, ImageUploader
+
   # TODO: Add 3 Validations (using 2 types)
   validates_presence_of :title, :created_by, :assigned_to
   validates_associated :created_by_user, :assigned_to_user
