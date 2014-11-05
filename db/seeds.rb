b01b91b970ca5388025acd519e5c2eed687b087a
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-05 01:15:02 -0700

    Updates seeds.rb to reflect new relationship mumbo jumbo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,44 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 joe = User.create!(email: 'joe@johnson.com', first_name: 'Joseph', last_name: 'Johnson', password: 'password', password_confirmation: 'password')
-joe.tasks.create([{
+mary = User.create!(email: 'mary@lamb.com', first_name: 'Mary', last_name: 'Lamb', password: 'password', password_confirmation: 'password')
+
+Task.create([{
                       title: "Some Task Here",
                       is_complete: true,
-                      description: "This is a great task. Please make sure to read this great description!"
+                      description: "This is a great task. Please make sure to read this great description!",
+                      created_by: joe.id,
+                      assigned_to: mary.id
                   },
                   {
                       title: "Another Task",
-                      is_complete: false
+                      is_complete: false,
+                      created_by: joe.id,
+                      assigned_to: joe.id
                   },
                   {
                       title: "Super Duper Task",
-                      is_complete: false
+                      is_complete: false,
+                      created_by: joe.id,
+                      assigned_to: mary.id
                   }])
 
-
-mary = User.create!(email: 'mary@lamb.com', first_name: 'Mary', last_name: 'Lamb', password: 'password', password_confirmation: 'password')
-mary.tasks.create([{
+Task.create([{
                       title: "Do the Dishes",
                       is_complete: false,
-                      description: "Dang them dishes are dirty!"
+                      description: "Dang them dishes are dirty!",
+                      created_by: mary.id,
+                      assigned_to: joe.id
                   },
                   {
                       title: "45 minute yoga session",
-                      is_complete: true
+                      is_complete: true,
+                      created_by: joe.id,
+                      assigned_to: mary.id
                   },
                   {
                       title: "Make TODO List",
-                      is_complete: false
+                      is_complete: false,
+                      created_by: joe.id,
+                      assigned_to: joe.id
                   }])
