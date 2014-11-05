c0cb441a7eb97ba57c6d76e81673851f39fce351
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-05 07:26:41 -0700

    Refactors from nested routes to non-nested for Tasks... much more!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "users"}
   devise_scope :user do
-    resources :users do
-      resources :tasks
-    end
+    resources :users
+    resources :tasks
   end
   root 'pages#home'
 end
