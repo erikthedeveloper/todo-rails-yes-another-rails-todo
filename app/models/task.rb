70d7d2bd23a4d41f8f11322fdcf4c2edd444740d
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-05 06:42:36 -0700

    Models - Removes TODOs/formatting

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,8 @@ class Task < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
-  # TODO: Add 3 Validations (using 2 types)
   validates_presence_of :title, :created_by, :assigned_to
   validates_associated :created_by_user, :assigned_to_user
-
-  # TODO: Implement functionality to upload image files for at least one of your resources using CarrierWave . Add a file field to the form for the resource, and display the uploaded image on the show page for the resource.
 
   def get_description
     description.empty? ? "No Description..." : description
